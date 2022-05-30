9f6a6a3e1c49b7574ab6e810b4a24b7b7a8abc00
qwertkorp <qwert.korp@gmail.com>
2022-05-30 12:28:54 +0300

    Project Task1

---
+++ Task1/DaysOfTheWeek/Program.cs
@@ -0,0 +1,53 @@
+﻿int dayNumber;
+
+Console.WriteLine("Приветствую!\nЭта программа выдает название дня недели по его порядковому номеру.");
+Console.WriteLine("Введите число от 1 до 7:");
+
+dayNumber = EnteringAndCheckingNumber();
+
+Console.Write("Этот день: ");
+
+DayOfTheWeekLogic(dayNumber);
+
+static void DayOfTheWeekLogic(int number)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    switch (number)
+    {
+        case 1:
+            Console.Write("Понедельник");
+            break;
+        case 2:
+            Console.Write("Вторник");
+            break;
+        case 3:
+            Console.Write("Среда");
+            break;
+        case 4:
+            Console.Write("Четверг");
+            break;
+        case 5:
+            Console.Write("Пятница");
+            break;
+        case 6:
+            Console.Write("Суббота");
+            break;
+        default:
+            Console.Write("Воскресенье");
+            break;
+    }
+    Console.ResetColor();
+}
+
+static int EnteringAndCheckingNumber()
+{
+    int enteredNumber;
+    
+    while (!int.TryParse(Console.ReadLine(), out enteredNumber) || enteredNumber < 1 || enteredNumber > 7)
+    {
+        Console.WriteLine("Введен не верный символю Введите число от 1 до 7:");
+    }
+    return enteredNumber;
+}
+
+
---
+++ Task1/MultiplicationTable/Program.cs
@@ -0,0 +1,33 @@
+﻿int firstNumber;
+int secondNumber;
+
+Console.WriteLine("Приветствую!\nПрограмма умножает введенное первое число на последовательность чисел от 1 до второго введенного числа.");
+
+Console.WriteLine("Введите первое число");
+firstNumber = EnteringAndCheckingNumber();
+
+Console.WriteLine("Введите второе число");
+secondNumber = EnteringAndCheckingNumber();
+MultiplicationTableLogic(firstNumber,secondNumber);
+
+static void MultiplicationTableLogic(int firstNumber,int secondNumber)
+{
+    long result;
+    
+    for(int i = 1; i <= secondNumber; i++)
+    {
+        result = firstNumber * i;
+        Console.WriteLine($"{firstNumber}*{i} = {result}");
+    }
+}
+
+static int EnteringAndCheckingNumber()
+{
+    int enterendNumber;
+
+    while(!int.TryParse(Console.ReadLine(), out enterendNumber) || 1 > enterendNumber || enterendNumber > int.MaxValue)
+    {
+        Console.WriteLine("Введен не верный символ или отрицательное число. Введите повторно:");
+    }
+    return enterendNumber;
+}
